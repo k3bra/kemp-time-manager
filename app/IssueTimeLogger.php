9db233dc91a14587fcaf2a2a5a99ae945374ae9b
Eduardo Domingues <eduardo.domingues@wezard.pt>
2017-08-07 08:47:44 +0100

    Add models

---
+++ app/IssueTimeLogger.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IssueTimeLogger extends Model 
+{
+
+    protected $table = 'issues_time_logger';
+    public $timestamps = false;
+
+}
