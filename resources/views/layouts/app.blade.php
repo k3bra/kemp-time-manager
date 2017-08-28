7c4eda7f6cb754b0e346c28529d93264ba903d4f
Eduardo <domingues.m.eduardo@gmail.com>
2017-08-28 19:56:07 +0100

    Update template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,79 +11,44 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     @yield('css')
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/side-menu.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/style.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/style-responsive.min.css')}}" rel="stylesheet"/>
+    <link href="{{ asset('css/blue.css') }}" rel="stylesheet" id="theme"/>
 </head>
 <body>
-    <div id="app">
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<div id="app">
 
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
 
-                <div class="navbar-header">
+    <div id="page-container" class=" page-sidebar-fixed page-header-fixed">
+        @include('layouts.topBar')
+        @include('layouts.sideBar')
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+        <div id="content" class="content">
+            <!-- begin breadcrumb -->
+            <ol class="breadcrumb pull-right">
+                <li><a href="javascript:;">Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
+            <!-- end breadcrumb -->
+            <!-- begin page-header -->
+            <h1 class="page-header">Dashboard
+                <small>header small text goes here...</small>
+            </h1>
+            <!-- end page-header -->
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+            @yield('content')
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li><a href="{{ url('/project') }}">See Projects</a></li>
-                            <li><a href="{{ url('/project/create') }}">Create Project</a></li>
-                            <li><a href="{{ url('/issue') }}">See Issue</a></li>
-                            <li><a href="{{ url('/issue/create') }}">Create issue</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        @yield('content')
+        </div>
+        <!-- end row -->
     </div>
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @yield('javascript')
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('javascript')
 
 </body>
 </html>
